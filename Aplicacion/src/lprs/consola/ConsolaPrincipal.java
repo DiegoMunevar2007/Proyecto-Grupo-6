ad1778a23d9241b7a23b186edce6e24c1247bd6f
DiegoMunevar2007 <d.munevar@uniandes.edu.co>
2024-10-17 21:57:11 -0500

    Cambio consola

--- Aplicacion/src/lprs/consola/ConsolaPrincipal.java
+++ Aplicacion/src/lprs/consola/ConsolaPrincipal.java
@@ -2,14 +2,11 @@ package lprs.consola;
 
 import java.util.Scanner;
 
-<<<<<<< HEAD
-=======
 import lprs.logica.cuentas.Estudiante;
 import lprs.logica.cuentas.Profesor;
 import lprs.logica.cuentas.Usuario;
 import lprs.principal.LPRS;
 
->>>>>>> 0bff5c85522313c07ad6e425aaa9995342a2277b
 public class ConsolaPrincipal {
 
 	Scanner lectura;
@@ -19,7 +16,7 @@ public class ConsolaPrincipal {
 	}
 
 	public void mostrarConsolaPrincipal() throws Exception {
-		//TODO: Cargar los usuarios a partir del JSON
+		// TODO: Cargar los usuarios a partir del JSON
 		System.out.println("Bienvenido a learning Path Recommendation System :) ");
 		System.out.println("Iniciar Sesion: 1 , Crear cuenta: 2");
 
@@ -27,31 +24,18 @@ public class ConsolaPrincipal {
 
 		int opcion = lectura.nextInt();
 
-		if (opcion == 1) 
-		{
-			Usuario usuarioEncontrado= iniciarSesion();
-			if (usuarioEncontrado.getTipo() == "Estudiante") 
-			{
+		if (opcion == 1) {
+			Usuario usuarioEncontrado = iniciarSesion();
+			if (usuarioEncontrado.getTipo() == "Estudiante") {
 				ConsolaEstudiante consolaEstudiante = new ConsolaEstudiante((Estudiante) usuarioEncontrado);
 				consolaEstudiante.mostrarConsolaEstudiante();
-			} 
-			else 
-			{
-				ConsolaProfesor consolaProfesor= new ConsolaProfesor((Profesor) usuarioEncontrado);
+			} else {
+				ConsolaProfesor consolaProfesor = new ConsolaProfesor((Profesor) usuarioEncontrado);
 			}
-			
+
 		} else if (opcion == 2) {
 			System.out.println("----------------Crear Usuario------------------");
 			System.out.println("Ingrese su Usuario: ");
-<<<<<<< HEAD
-		
-			//String usuario = lectura.next();
-	
-			System.out.println("Ingrese su contraseña: ");
-	
-			//String contrasena = lectura.next();
-			
-=======
 
 			String usuario = lectura.next();
 
@@ -59,12 +43,11 @@ public class ConsolaPrincipal {
 
 			String contrasena = lectura.next();
 
->>>>>>> 0bff5c85522313c07ad6e425aaa9995342a2277b
 			System.out.println("Estudiante: 1 , Profesor: 2 ");
 
 			int tipo = lectura.nextInt();
-			LPRS.crearUsuario(usuario, contrasena, tipo);  
-			
+			LPRS.crearUsuario(usuario, contrasena, tipo);
+
 			System.out.println("Usuario agregado con exito yipeee");
 			mostrarConsolaPrincipal();
 
@@ -77,9 +60,10 @@ public class ConsolaPrincipal {
 	public void mostrarOpciones(int cantidad, String[] opciones) {
 		System.out.println("Seleccione una opcion: ");
 		for (int i = 0; i < cantidad; i++) {
-			System.out.println(i+1 + ". " + opciones[i]);
+			System.out.println(i + 1 + ". " + opciones[i]);
 		}
 	}
+
 	public Usuario iniciarSesion() throws Exception {
 		System.out.println("----------------Iniciar Sesion------------------");
 
@@ -87,11 +71,11 @@ public class ConsolaPrincipal {
 
 		String usuarioID = lectura.next();
 		Usuario usuarioEncontrado = LPRS.obtenerUsuario(usuarioID);
-		
+
 		System.out.println("Ingrese su contraseña: ");
 		String contrasena = lectura.next();
 		return LPRS.iniciarSesion(usuarioEncontrado, contrasena);
 
 	}
-	
+
 }
